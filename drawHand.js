1efe0d88b08588900f5587c5f6e996aa86a987ce
milon8561 <michael3419371@gmail.com>
2023-10-17 22:49:11 -0400

    yoyoyoyo

--- drawHand.js
+++ drawHand.js
@@ -5,6 +5,7 @@ import { removeImages } from "./removeImages.js";
 import { deck, deck_html, deckDisplay_html, hand, hand_html, lostzone, lostzone_html, discard, discard_html, stadium, stadium_html, prizes, 
 prizes_html, active, active_html, bench, bench_html, cardData, prizesHidden_html, lostzoneDisplay_html, discardDisplay_html } from "./initialization.js";
 import { allowDrop, drop, dragStart, dragEnd } from "./drag.js";
+import { socket } from "./frontend.js";
 
 // Draw starting hand of 7
 export function drawHand(){
@@ -41,7 +42,7 @@ export function drawHand(){
         shuffle(deck.cards, deck.images);
         // Append the <img> element to the deck display modal
         const coverImage = document.createElement('img');
-        coverImage.src = 'cardScans/cardback.png';
+        /* coverImage.src = 'cardScans/cardback.png';
         coverImage.id = "deckCover"; // id to reference for dropping
         coverImage.addEventListener("dragover", allowDrop);
         coverImage.addEventListener("drop", drop);
@@ -52,9 +53,40 @@ export function drawHand(){
         // allow card to be dragged to draw the top card of the deck
         coverImage.draggable = true; // Make image draggable
         coverImage.addEventListener('dragstart', dragStart); //Add a dragstart event listener
-        coverImage.addEventListener('dragend', dragEnd);   
+        coverImage.addEventListener('dragend', dragEnd); */
+
+        const imageAttributes = {
+            src: 'cardScans/cardback.png',
+            id: 'deckCover',
+            // Event listener for drag-and-drop
+            dragover: allowDrop,
+            drop: drop,
+            // Function to open the modal when clicked
+            click: () => {
+              deck_html.style.display = 'block';
+            },
+            // Make the image draggable
+            draggable: true,
+            // Event listeners for drag-and-drop functionality
+            dragstart: dragStart,
+            dragend: dragEnd
+        };
+          
+        // Set image attributes and event listeners
+        for (const attr in imageAttributes) {
+            if (typeof imageAttributes[attr] === 'function') {
+                // If it's a function (an event listener), add it as an event listener
+                coverImage.addEventListener(attr, imageAttributes[attr]);
+            } else {
+                // Otherwise, set it as an attribute
+                coverImage.setAttribute(attr, imageAttributes[attr]);
+            };
+        }
 
         deckDisplay_html.appendChild(coverImage);
+
+        socket.emit('appendImage', imageAttributes);
+
 
         for (let i = 0; i < deck.count; i++){
             deck_html.appendChild(deck.images[i]);
