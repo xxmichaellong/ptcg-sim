52688ac0ccfbaeb5e21d0244f0c2e53bc5f5c03a
milon8561 <michael3419371@gmail.com>
2024-01-13 16:18:27 -0500

    donations link+arielle is da best

--- src/js/front-end/initialization/document-event-listeners/sidebox/p1/changelog.js
+++ src/js/front-end/initialization/document-event-listeners/sidebox/p1/changelog.js
@@ -1,10 +1,13 @@
 export const initializeChangelog = () => {  
+    const donationsPage = document.getElementById('donationsPage');
+
     const changelogLink = document.getElementById('changelogLink');
     changelogLink.addEventListener('click', () => {
         if (changelog.style.display === 'block') {
             changelog.style.display = 'none';
         } else {
             changelog.style.display = 'block';
+            donationsPage.style.display = 'none';
         };
     });
     
