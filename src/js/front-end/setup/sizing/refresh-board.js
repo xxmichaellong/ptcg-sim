668dcfe198e8107b35f7fe107f5d67e2674786dd
milon8561 <michael3419371@gmail.com>
2024-02-27 11:33:03 -0500

    image refresh patch

--- src/js/front-end/setup/sizing/refresh-board.js
+++ src/js/front-end/setup/sizing/refresh-board.js
@@ -6,29 +6,33 @@ import { adjustCards } from "./resizer.js";
 const refreshZone = (user, zoneId) => {
     const zone = getZone(user, zoneId);
     //find all playContainers
-    const playContainers = zone.element.querySelectorAll('DIV')
+    const playContainers = zone.element.querySelectorAll('DIV');
     //loop through each box
     playContainers.forEach((playContainer) => {
         //find all images within box
         const images = (playContainer).querySelectorAll('img');
         //loop through each image and update the attached cards
         images.forEach((image) => {
-            if (!image.attached){
-                //re-append the card to the end of the same zone
-                let currentRotation;
-                if (image.PokémonBreak){
-                    currentRotation = (parseInt(image.style.transform.replace(/[^0-9-]/g, '')) || 0) - 90;
-                } else {
-                    currentRotation = parseInt(image.style.transform.replace(/[^0-9-]/g, '')) || 0;
-                };
-                const numberRotations = currentRotation / 90;
-                const index = zone.array.findIndex(card => card.image === image);
-                moveCard(user, user, zoneId, zoneId, index);
-                const newIndex = zone.array.findIndex(card => card.image === image);
-                for (let i = 0; i < numberRotations; i ++){
-                    rotateCard(user, zoneId, newIndex, false, false);
+            const img = new Image();
+            img.onload = () => {
+                if (!image.attached){
+                    //re-append the card to the end of the same zone
+                    let currentRotation;
+                    if (image.PokémonBreak){
+                        currentRotation = (parseInt(image.style.transform.replace(/[^0-9-]/g, '')) || 0) - 90;
+                    } else {
+                        currentRotation = parseInt(image.style.transform.replace(/[^0-9-]/g, '')) || 0;
+                    };
+                    const numberRotations = currentRotation / 90;
+                    const index = zone.array.findIndex(card => card.image === image);      
+                    moveCard(user, user, zoneId, zoneId, index);
+                    const newIndex = zone.array.findIndex(card => card.image === image);
+                    for (let i = 0; i < numberRotations; i ++){
+                        rotateCard(user, zoneId, newIndex, false, false);
+                    };
                 };
             };
+            img.src = image.src;
         });
     });
     adjustCards(user, zoneId, 1);
@@ -41,7 +45,6 @@ export const refreshBoard = () => {
         ['opp', 'active'],
         ['opp', 'bench'],
     ];
-
     zones.forEach(([user, zoneId]) => {
         refreshZone(user, zoneId);
     });
